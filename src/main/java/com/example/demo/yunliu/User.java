f511e3e481101f0982c80b87ec3a5046f89c12e6
yunliu <695417972@qq.com>
2021-05-19 16:47:58 +0800

    2021/05/19 submit user to git

---
+++ src/main/java/com/example/demo/yunliu/User.java
@@ -0,0 +1,14 @@
+package com.example.demo.yunliu;
+
+import lombok.Data;
+
+/**
+ * @Auther: yunliu
+ * @Date: 2021/5/19 16:44
+ * @Description:
+ */
+@Data
+public class User {
+
+    private Integer userName;
+}
